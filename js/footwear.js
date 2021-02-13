cb7e8a8cbcb7702c2f0cae98e4a7f2948f758f28
joshbrereton <bre21001@byui.edu>
2021-02-12 22:23:07 -0700

    done

---
+++ js/footwear.js
@@ -0,0 +1,20 @@
+// Input: Receive the input of one of the 4 weather selections from User.
+// Processing: Depending on the answer given by user, compute the answer to the correct selection of Footwear.
+// Output: Output the correct footwear for the weather condition.
+function weatherConverter() {
+    let weatherSelect = document.getElementById('weatherSelect').value;
+    var message;
+    if (weatherSelect == 'hot') {
+        message = "Sandles";
+    }
+    else if (weatherSelect == 'rain') {
+        message = "Galoshes";
+    }
+    else if (weatherSelect == 'snow') {
+        message = "Boots";
+    }
+    else (weatherSelect == 'unknown') {
+        message = "Shoes"
+    }
+   document.getElementById('output').innerHTML = message; 
+}
