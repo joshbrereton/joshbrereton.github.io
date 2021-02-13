3d1c34cd7ef0e40deb7f22077689ea9046d8f85b
joshbrereton <bre21001@byui.edu>
2021-02-13 14:46:04 -0700

    done

---
+++ js/calculator.js
@@ -0,0 +1,31 @@
+// Input: Receive the input of the first and second number, with the arithmetic symbol and the answer
+// Processing: Compute the correct answer given the input from the user
+// Output: Output if the user is correct or not
+function calculator () {
+    let number1 = parseFloat(document.getElementById('number1').value);
+    let number2 = parseFloat(document.getElementById('number2').value);
+    let answer = parseFloat(document.getElementById('answer').value);
+    let correctAnswer;
+    switch (problem) {
+      case '+':
+      correctAnswer = number1 + number2;
+      break;
+      case '-':
+      correctAnswer = number1 - number2;
+        break;
+        case '/':
+        correctAnswer = number1 / number2;
+        break;
+        case '*':
+        correctAnswer = number1 * number2;
+        break;
+    }
+        let message;
+        if (correctAnswer == 'answer') {
+          message = "Correct! 🏆";
+        }
+          else {
+            message = "incorrect 😢";
+            document.getElementById('output').innerHTML = message;
+          }
+        }
---
+++ js/footwear.js
@@ -0,0 +1,22 @@
+// Input: Receive the input of one of the 4 weather selections from User.
+// Processing: Depending on the answer given by user, compute the answer to the correct selection of Footwear.
+// Output: Output the correct footwear for the weather condition.
+function weatherConverter () {
+let message;
+let weather = document.getElementById('weather').value;
+switch (weather) {
+case hot:
+message = "Sandles";
+break;
+case rain:
+message = "Galoshes";
+break;
+case snow:
+message = "Boots";
+break;
+case unknown:
+message = "Shoes";
+break;
+}
+document.getElementById('output').innerHTML = "The right footwear is " + message;
+}
