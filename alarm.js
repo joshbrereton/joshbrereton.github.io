5bb3d157cd97bc98032c1ee5082e1fd684d0938c
joshbrereton <bre21001@byui.edu>
2021-02-18 22:41:11 -0700

    Done

---
+++ alarm.js
@@ -0,0 +1,11 @@
+ var now = new Date();
+    var month = now.getMonth();
+    var dayOfMonth = now.getDate();
+    var dayOfWeek = now.getDay();
+let message;
+if (1 <= dayOfWeek && dayOfWeek <= 5) {
+    message = 'Get Up!';
+} else if ((6 == dayOfWeek || dayOfWeek == 0) && (month == 0 && dayOfWeek == 1) && (month == 6 && dayOfWeek == 4) && (month == 11 && dayOfWeek == 25)) {
+    message = 'Sleep in!';
+}
+document.getElementById('output').innerHTML = message;
