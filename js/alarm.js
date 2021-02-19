88e0294d9331cebfa286c95314d20792beb60a29
joshbrereton <bre21001@byui.edu>
2021-02-18 23:02:22 -0700

    done

--- js/alarm.js
+++ js/alarm.js
@@ -2,7 +2,7 @@
     var month = now.getMonth();
     var dayOfMonth = now.getDate();
     var dayOfWeek = now.getDay();
-let message;
+    var message;
 if (1 <= dayOfWeek && dayOfWeek <= 5) {
     message = 'Get Up!';
 } else if ((6 == dayOfWeek || dayOfWeek == 0) && (month == 0 && dayOfWeek == 1) && (month == 6 && dayOfWeek == 4) && (month == 11 && dayOfWeek == 25)) {
