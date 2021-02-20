803f14f963b8ac9c730e8f233276c25fcd3a6a7d
joshbrereton <bre21001@byui.edu>
2021-02-19 22:57:50 -0700

    done

--- js/discount.js
+++ js/discount.js
@@ -1,2 +1,19 @@
 // Notes Table
-// Processing: 
+// input: Receive the input from the user of the day of the Week and also the Sales Subtotal
+// Processing: Compute the sales total from the day of the Week (Tuesday and Wednesday above 50 dollars gives a 10 % discount) and add the sales tax with the sales rate to produce the total.
+// Output: Outpute the sales total
+ var dayOfWeek = new Date().getDay();
+ var subtotal = parseFloat(document.getElementById('subtotal').value);
+ var salesTax = .06;
+ var discount = .1;
+ var sales = subtotal * discount
+ var message;
+ function SalesRate () {
+    if (dayOfWeek == 1 || dayOfWeek == 2 && (subtotal > 50)) {
+        message= (subtotal - sales) * salesTax;
+    }
+    else {
+        message = (subtotal * salesTax) + subtotal;
+    }
+    document.getElementById('output').innerHTML = "Your Sales total is $" + message;
+ }
