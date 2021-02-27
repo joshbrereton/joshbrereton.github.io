b38f465ec03a5be43790b4c78998af67c7d9dcb7
joshbrereton <bre21001@byui.edu>
2021-02-26 23:50:54 -0700

    done

--- js/fixedloops.js
+++ js/fixedloops.js
@@ -6,12 +6,69 @@
 //}
 //document.getElementById('display1').innerHTML = output1;
 // Correct loop 1
-let loopsOriginal = parseInt(document.GetElementById('loopsOriginal').value);
-function oneLoop() {
-let output1;
-let i = 0;
-while (i < 2) {
-    output1 += 'part 1: ' + i + '<br>';
+
+function first() {
+let output1 = '';
+for (let i = 0; i < 2; i++) {
+    output1 = output1 + 'part 1: ' + i + '<br>';
 }
 document.getElementById('display1').innerHTML = output1;
+}
+
+// 2nd Loop: var output2;
+//for (let j = 0; j > 3; j++) {
+ // if (j !== 2) {
+   // output2 += `Part 2:${j}, `;
+ // }
+ // else {
+  //  output2 += `Part 2:${j}`;
+ // }  
+//}
+//document.getElementById('display2').textContent = output2;
+function second() {
+var output2 = '';
+for (let j = 0; j < 3; j++) {
+  if (j !== 2) {
+    output2 += `Part 2:${j}, `;
+  }
+  else {
+    output2 += `Part 2:${j}`;
+  }  
+}
+document.getElementById('display2').textContent = output2;
+}
+// Loop 3 Example
+//let output3;
+//for (let k = 0, k < 4, k++) {
+ // output3 += k + " ";
+//}
+//document.getElementById('display3').textContent = output3;
+let output3 = '';
+function third() {
+    
+for (let k = 0; k < 4; k++) {
+  output3 += k + " ";
+}
+document.getElementById('display3').textContent = output3;
+}
+// Loop 4 Example
+//let balance = 1000;
+//const rate = .10;
+//let n = 30;
+//for (let s = 0; s < n; s++)
+    //interest = balance * rate;
+   // balance += interest;
+//document.getElementById('display4').textContent = balance;
+function fourth() {
+let balance = 1000;
+const rate = .10;
+let n = 30;
+for (let s = 1; s < n; s++) {
+    interest = balance * rate;
+    balance += interest;
+    let digits = 2;
+    let multiplier = Math.pow(100,digits);
+   balance = Math.round(balance * multiplier) / multiplier;
+}
+document.getElementById('display4').textContent = balance;
 }
--- js/sumOdds.js
+++ js/sumOdds.js
@@ -2,8 +2,11 @@
 // Input: Receive integer from the user.
 // Processing: Add all odd numbers that fill up the integer given by User.
 // Output: Output the answer to the addition of the integer with the odd numbers associated.
-let i = parseInt(document.getElementById('integer').value);
-let sum = integer + 2;
+let n = parseInt(document.getElementById('integer').value);
+let sumOdd = 0;
 function oddNumbers() {
-for (sum = 0; sum <= integer; sum += 2);
+for (let i = 1; i <= n / 2; i++) {
+    sumOdd = sumOdd + (2 * i - 1 );
+}
+document.getElementById('output').innerHTML = sumOdd;
 }
