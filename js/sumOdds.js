a4439272474bbd0a5f997d2ade9757962075d4ce
joshbrereton <bre21001@byui.edu>
2021-03-03 13:49:37 -0700

    done

--- js/sumOdds.js
+++ js/sumOdds.js
@@ -2,11 +2,19 @@
 // Input: Receive integer from the user.
 // Processing: Add all odd numbers that fill up the integer given by User.
 // Output: Output the answer to the addition of the integer with the odd numbers associated.
-let n = parseInt(document.getElementById('integer').value);
-let sumOdd = 0;
 function oddNumbers() {
-for (let i = 1; i <= n / 2; i++) {
-    sumOdd = sumOdd + (2 * i - 1 );
+ let n = parseInt(document.getElementById('integer').value);
+ let k;
+let sumOdd = 0;
+if (n % 2 == 0) {
+    k = n / 2;
+}
+else {
+    k = (n + 1) / 2;
+}
+for (let i = 1; i <= k; i++) {
+  sumOdd = sumOdd + (2 * i  - 1);
+
 }
 document.getElementById('output').innerHTML = sumOdd;
 }
