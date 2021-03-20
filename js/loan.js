5f022f70b2df4a3058f3a8e170d7472eb535ac6c
joshbrereton <bre21001@byui.edu>
2021-03-19 23:57:14 -0600

    done

---
+++ js/loan.js
@@ -0,0 +1,42 @@
+/* Input: Receive Input from User
+Processing: Calculate Monthly payment Loan with an Interest with the function: p =  ar / 1 − (1 + r) raised to 
+the -nth power.
+ Then calculate the payoff amount with the following function: 
+ b = a(1 + r)raised d −  p ( (1 + r)raised d − 1 ) / r.
+ Output: Output the payment per month Loan and the payoff amount at the end to the user.
+*/
+function doPayment() {
+    let principal = parseFloat(document.getElementById('amountBorrowed').value);
+    let annualRate = parseFloat(document.getElementById('annualInterestRate').value);
+    let years = parseFloat(document.getElementById('numberOfYears').value);
+    let periodsPerYear = parseFloat(document.getElementById('paymentsPerYear').value);
+    let p = computePayment(principal, annualRate, years, periodsPerYear);
+    document.getElementById('output').innerHTML = p;
+}
+function computePayment(principal, annualRate, years, periodsPerYear) {
+let a = principal;
+let r = annualRate / 12;
+let n = years * 12;
+p = (a) * (r) / (1) - Math.pow(1 + r, -n);
+p = Math.round(p * 100) / 100;
+return p;
+}
+function doBalance() {
+    let principal = parseFloat(document.getElementById('amountBorrowed').value);
+    let annualRate = parseFloat(document.getElementById('annualInterestRate').value);
+    let years = parseFloat(document.getElementById('numberOfYears').value);
+    let periodsPerYear = parseFloat(document.getElementById('paymentsPerYear').value)
+    let numberOfPaymentPaidToDate = parseFloat(document.getElementById('paymentsPaid').value);
+    let b = computeBalance(principal, annualRate, years, periodsPerYear, numberOfPaymentPaidToDate);
+    document.getElementById('output').innerHTML = b;
+}
+function computeBalance(principal, annualRate, years, periodsPerYear, numberOfPaymentPaidToDate) {
+let d = numberOfPaymentPaidToDate;
+let r = annualRate / 12;
+let a = principal;
+let n = years * 12;
+let p;
+let b = a * Math.pow(1 + r, d) - p (Math.pow(1 + r, d) - 1) / r;
+b = Math.round(f * 100) / 100;
+return b;
+}
