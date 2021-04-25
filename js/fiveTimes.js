06157e6d669a7a9ecb6276367f507bd969663ad3
joshbrereton <bre21001@byui.edu>
2021-04-24 22:39:53 -0600

    School Work

--- js/fiveTimes.js
+++ js/fiveTimes.js
@@ -3,11 +3,11 @@ input: Have user know the times 5 table.
 processing: 5 times table from 1 to 12.
 Output: Output the 5 times table from 1 to 12 to User.*/
 function fiveTimes() {
-let sum = 5;
+let sum = "5";
 let output = '';
  for (let i = 1; i <= 12; i++) {
      let multiplier = sum * i;
-     output += sum + " * " + i + " = " + multiplier + "<br>";
+     output += sum + " * " +  + i + " = " + multiplier + "<br>";
  }
  document.getElementById('output').innerHTML = output;
 }
