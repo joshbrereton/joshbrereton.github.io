b87cb9f9781e5ce63310a7a8511fa0385f6b84a7
joshbrereton <bre21001@byui.edu>
2021-05-29 23:58:44 -0600

    working

---
+++ WDD230/lesson6/js/windchill.js
@@ -0,0 +1,6 @@
+let t = document.querySelector('#temp');
+let w = document.querySelector('#wind');
+let f = document.querySelector('#span');
+if (temp <= 50 || wind > 3) {
+f = 35.74  +0.6215t −35.75 s * 0.16  +0.4275t s 0.16
+}
