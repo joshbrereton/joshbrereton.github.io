3fc43b93f9f283a30a2bac4bba1867ef786354c2
joshbrereton <bre21001@byui.edu>
2021-06-03 23:57:26 -0600

    window

---
+++ WDD230/lesson7/js/imageHandling.js
@@ -0,0 +1,30 @@
+const imagesToLoad = document.querySelectorAll('image[data-src]');
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+const imgOptions = {
+    rootMargin: '0px 0px 50px 0px',
+    threshold: 0
+};
+
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver(items => {
+        if (item.isIntersecting) {
+            loadImages(item.target);
+            imgObserver.unobserve(item.target);
+        }
+    });
+}, imgOptions);
+
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+});
+
+} else {
+    imagesToLoad.forEach(img => {
+        loadImages(img);
+    });
+}
