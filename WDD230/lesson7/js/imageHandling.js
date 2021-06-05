c5935924711088c318fcf5ddbe0fec6e76d3431a
joshbrereton <bre21001@byui.edu>
2021-06-05 15:33:44 -0600

    complete lazy loading

--- WDD230/lesson7/js/imageHandling.js
+++ WDD230/lesson7/js/imageHandling.js
@@ -1,4 +1,4 @@
-const imagesToLoad = document.querySelectorAll('image[data-src]');
+const imagesToLoad = document.querySelectorAll('img[data-src]');
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
     image.onload = () => {
@@ -6,21 +6,22 @@ const loadImages = (image) => {
     };
 };
 const imgOptions = {
-    rootMargin: '0px 0px 50px 0px',
-    threshold: 0
+    rootMargin: '0px 0px 10px 0px',
+    threshold: 1
 };
 
 if ('IntersectionObserver' in window) {
     const imgObserver = new IntersectionObserver(items => {
+        items.forEach(item => {
         if (item.isIntersecting) {
             loadImages(item.target);
             imgObserver.unobserve(item.target);
         }
     });
+}, imgOptions);
 
-
-imagesToLoad.forEach((img) => {
-    loadImages(img);
+imagesToLoad.forEach(img => {
+    imgObserver.observe(img);
 });
 
 } else {
