e436c78e3b8e3b750d715e37a982b7597e238fd5
joshbrereton <bre21001@byui.edu>
2021-06-19 22:48:22 -0600

    complete

--- WDD230/lesson9/js/prophets.js
+++ WDD230/lesson9/js/prophets.js
@@ -4,16 +4,27 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject); // Just to see if this works on Console.
+    //console.table(jsonObject); // Just to see if this works on Console.
+    const prophets = jsonObject['prophets'];
     for (let i = 0; i < prophets.length; i++) {
       let card = document.createElement('section');
-let h2 = document.createElement('h2');
+      let h2 = document.createElement('h2');
+      let p1 = document.createElement('p');
+      let p2 = document.createElement('p');
+      let image = document.createElement('img');
 
-h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+      h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+      p1.innerHTML = prophets[i].birthdate;
+      p2.innerHTML = prophets[i].birthplace;
+      image.setAttribute('src', prophets[i].imageurl);
+      image.setAttribute('alt', "The official portrait of" + "prophets[i].name" + ' ' + "prophets[i].lastname");
+      image.style.boxShadow = '0 0 30px #999';
 
-card.appendChild(h2);
+      card.appendChild(h2);
+      card.appendChild(p1);
+      card.appendChild(p2);
+      card.appendChild(image);
 
-document.querySelector('div.cards').appendChild(card);
+      document.querySelector('div.cards').appendChild(card);
     }
   });
-  const prophets = jsonObject['prophets'];
