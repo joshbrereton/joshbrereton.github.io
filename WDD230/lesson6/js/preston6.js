d746ae12867f91b1339bc50cbbd89bf58545bf76
joshbrereton <bre21001@byui.edu>
2021-07-03 23:58:28 -0600

    done

--- WDD230/lesson6/js/preston6.js
+++ WDD230/lesson6/js/preston6.js
@@ -1,8 +1,23 @@
-function toggleMenu() {
-    document.getElementsByClassName('navigation')[0].classList.toggle('responsive');
-}
-document.querySelector('#lastupdate').textContent = document.lastModified;
-let d = new Date();
-if (d.getDay() == 5) {
-    document.querySelector('aside').style.display = "block";
-}
+let last;
+if (localStorage.getItem('date') !== null) {
+    last = new Date(localStorage.getItem('date')); }
+    else {
+        last = new Date();
+        localStorage.setItem('date', String(last));
+    }
+    document.getElementById('dayssince').innerHTML = String(last);
+    
+let now = new Date();
+
+const oneDay = 1000 * 60 * 60 * 24;
+const milliseconds = now - last;
+
+// Calculating the time difference between two dates
+//const diffInTime = date2.getTime() - date1.getTime();
+
+// Calculating the no. of days between two dates
+//const diffInDays = Math.round(diffInTime / oneDay);
+
+document.getElementById('dayssince').innerHTML = Math.trunc(milliseconds / oneDay) + '' + 'days ago';
+
+localStorage.setItem('date', String(now));
