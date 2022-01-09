ea72da4f6be13bbbf4d2064f816caf9eacf59eb3
josh brereton <joshbrereton@hotmail.com>
2022-01-08 23:44:51 -0700

    done

---
+++ WDD230/favorite-color.py
@@ -0,0 +1,8 @@
+color = input('Please type your favorite color!')
+# it will print in the console to ask the user to enter their color.
+print('Wow that is awesome that your favorite color is')
+# Tell the user what their favorite color is.
+print(color)
+# Grab the variable color that they used.
+print('My favorite color is...')
+
