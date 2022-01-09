9227b8505ab7a7c93dfeef5a7f97c3684f94615b
josh brereton <joshbrereton@hotmail.com>
2022-01-08 23:49:18 -0700

    Done with program

--- WDD230/favorite-color.py
+++ WDD230/favorite-color.py
@@ -5,4 +5,8 @@ print('Wow that is awesome that your favorite color is')
 print(color)
 # Grab the variable color that they used.
 print('My favorite color is...')
+sport = input('What sport do you like to play the most?')
+print('You will become famous one day, because your favorite sport is')
+print(sport)
+
 
